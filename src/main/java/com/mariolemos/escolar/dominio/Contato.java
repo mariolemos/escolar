8ac593d04430af2bbcfb88c0eab32f64223269cf
mariolemos <lemosmm64@gmail.com>
2021-10-07 14:54:42 -0300

    Testando a primeira classe de dominio

---
+++ src/main/java/com/mariolemos/escolar/dominio/Contato.java
@@ -0,0 +1,72 @@
+package com.mariolemos.escolar.dominio;
+
+import java.io.Serializable;
+
+public class Contato implements Serializable {	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	private String contato;
+	private String tipo;
+	
+	public Contato() {
+		
+	}
+	
+	public Contato(Integer id, String contato, String tipo) {
+		
+		this.id = id;
+		this.contato = contato;
+		this.tipo = tipo;		
+	}
+	
+	public Integer getId() {
+		return id;
+	}
+	
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	public String getContato() {
+		return contato;
+	}
+	
+	public void setContato(String contato) {
+		this.contato = contato;
+	}
+	
+	public String getTipo() {
+		return tipo;
+	}
+	
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
--- src/main/java/com/mariolemos/escolar/resources/ContatoResource.java
+++ src/main/java/com/mariolemos/escolar/resources/ContatoResource.java
@@ -1,15 +1,28 @@
 package com.mariolemos.escolar.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.mariolemos.escolar.dominio.Contato;
 
 @RestController
 @RequestMapping(value="/contatos")
 public class ContatoResource {
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "Rest está fucionando!";
+	public List<Contato>listar() {
+		
+		Contato cont1 = new Contato(1, "71988322598", "Telefone");
+		Contato cont2 = new Contato(2, "lemosmm@bol.com.br", "e-mail");
+		
+		List<Contato> lista = new ArrayList<>();
+		lista.add(cont1);
+		lista.add(cont2);
+		
+		return lista;
 	}
-
+	
 }
