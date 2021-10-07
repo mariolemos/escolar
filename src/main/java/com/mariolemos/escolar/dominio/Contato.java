2f49670da5b7e0a3eb65939563489c5e70beb0cb
mariolemos <lemosmm64@gmail.com>
2021-10-07 16:01:31 -0300

    Criação da base de Dados Automática

--- src/main/java/com/mariolemos/escolar/dominio/Contato.java
+++ src/main/java/com/mariolemos/escolar/dominio/Contato.java
@@ -2,9 +2,18 @@ package com.mariolemos.escolar.dominio;
 
 import java.io.Serializable;
 
+import javax.annotation.Generated;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Contato implements Serializable {	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String contato;
 	private String tipo;
