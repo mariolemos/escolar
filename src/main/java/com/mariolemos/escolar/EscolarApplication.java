5aadd5b6619c29355927fc3780e6f65401082fd9
mariolemos <lemosmm64@gmail.com>
2021-10-10 10:51:43 -0300

    Povoando a base de dados automaticamente

--- src/main/java/com/mariolemos/escolar/EscolarApplication.java
+++ src/main/java/com/mariolemos/escolar/EscolarApplication.java
@@ -1,13 +1,35 @@
 package com.mariolemos.escolar;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mariolemos.escolar.Repositories.ContatoRepository;
+import com.mariolemos.escolar.dominio.Contato;
+
 @SpringBootApplication
-public class EscolarApplication {
+public class EscolarApplication implements CommandLineRunner {
+	
+	@Autowired
+	private ContatoRepository contatoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(EscolarApplication.class, args);		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Contato cont1 = new Contato(null, "988322598", "Telefone");
+		Contato cont2 = new Contato(null, "lemosmm@bol.com.br", "E-mail");
+		Contato cont3 = new Contato(null, "mariolemos64", "Instagram");
+		
+		contatoRepository.saveAll(Arrays.asList(cont1, cont2, cont3));
+		
+		
 	}
 
 }
